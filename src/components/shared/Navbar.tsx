acf4cafa4fc7940b7df56f9317ae931f50d58e8d
rafi <rafiahmedrabby282@gmail.com>
2025-09-30 12:05:07 +0600

    setup navbar

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar03 } from '../ui/shadcn-io/navbar-03'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Navbar03/>
+    </div>
+  )
+}
+
+export default Navbar
